2b9a0dc16daa96155c7fc8ce7fd32b54725786e8
LoganGandy <logan@gandyeyecare.com>
2017-11-20 21:36:07 -0600

    master

---
+++ RanchNet1.1/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  RanchNet1.1
+//
+//  Created by Logan Gandy on 10/23/17.
+//  Copyright © 2017 Logan Gandy. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
