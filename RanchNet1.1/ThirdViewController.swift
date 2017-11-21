2bd3f1175f2001bc71f03d773c2388b94a672845
LoganGandy <logan@gandyeyecare.com>
2017-11-20 21:40:33 -0600

    test

--- RanchNet1.1/ThirdViewController.swift
+++ RanchNet1.1/ThirdViewController.swift
@@ -13,6 +13,8 @@ class ThirdViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // changes
     }
     
     override func didReceiveMemoryWarning() {
